128fd6f87c8e5f393c27c6252f4da6a1f8350156
Marina Travacio <48090259+mtravacio@users.noreply.github.com>
2019-07-16 13:25:42 -0300

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,16 @@
+Power <- read.table(file = "household_power_consumption.txt",
+                    sep =";",
+                    header = TRUE,
+                    stringsAsFactors = FALSE)
+
+library(tidyverse)
+
+Power_subset <- Power %>% filter(Date == '1/2/2007' | Date == '2/2/2007')
+
+plot1 <- hist(as.numeric(as.character(Power_subset$Global_active_power)),
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+
