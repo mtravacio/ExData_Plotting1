f605cf725043261317516501c581a65bdd4f0cd0
Marina Travacio <48090259+mtravacio@users.noreply.github.com>
2019-07-16 16:13:44 -0300

    Plot 1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+#Load data
 Power <- read.table(file = "household_power_consumption.txt",
                     sep =";",
                     header = TRUE,
@@ -6,12 +7,15 @@ Power <- read.table(file = "household_power_consumption.txt",
 
 library(tidyverse)
 
+#Subset data
 Power_subset <- Power %>% filter(Date == '1/2/2007' | Date == '2/2/2007')
 
+#Make plot
 plot1 <- hist(as.numeric(as.character(Power_subset$Global_active_power)),
      col = "red",
      main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)")
 
+#Save plot as png file
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
